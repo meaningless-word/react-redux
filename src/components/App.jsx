84ffe86156ce85a369cbd0a8a8aafad67d1c374d
meaningless-word <grisly@mail.ru>
2024-04-17 23:42:09 +0700

    добавление миддлвара

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,7 +9,7 @@ class App extends React.Component {
   }
 
   render() {
-    console.log(this.props);
+    // console.log(this.props);
     return (
       <div>
         <input
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom"; // для старой концепции
 // import { createRoot } from "react-dom/client"; // для новой концепции
 
 // ВС-код ругается на древность криэйтСтора, поэтому импортируется легаси-версия
-import { legacy_createStore as createStore } from "redux";
+// добавили поддержку миддлваров
+import { legacy_createStore as createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 
 import App from "./components/App.jsx";
 import reducer from "./redux/reducers/index.jsx";
+import { logging } from "./redux/middlewares/logging.jsx";
 
 // на этом шаге усложняется редьюсер - он становится комбинированным
 // усложним структуру стора - станет объектом с полями-массивами
@@ -20,7 +22,8 @@ import reducer from "./redux/reducers/index.jsx";
 // а вместо него мы импортнули комбинированный
 
 // объявление стора с указанием метода, которым он будет обрабатывать экшены
-const store = createStore(reducer);
+// вторым аргументом передаётся объект поддержки миддлваров, параметром которого становится наша функция
+const store = createStore(reducer, applyMiddleware(logging));
 
 // это типа современный синтаксис внедрения компонента приложения
 // const rootElement = document.getElementById("root");
