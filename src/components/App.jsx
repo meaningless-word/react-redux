4a8d0d216afd399d77cee554194337ffa6977d61
meaningless-word <grisly@mail.ru>
2024-04-17 18:23:57 +0700

    странно, но почему-то заработало.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,31 +6,31 @@ import { connect } from "react-redux";
 // }
 
 // от функционального представления к классовому
-class App extends React.Component {
-  render() {
-    console.log(this.props);
-    return <div>Hi</div>;
-  }
-}
-
-//export default App;
-
 // class App extends React.Component {
 //   render() {
 //     console.log(this.props);
-//     return (
-//       <div>
-//         <input type="text" />
-//         <button>click</button>
-//         <ul>
-//           {this.props.testStore.map((item) => (
-//             <li key={item}>{item}</li>
-//           ))}
-//         </ul>
-//       </div>
-//     );
+//     return <div>Hi</div>;
 //   }
 // }
+
+//export default App;
+
+class App extends React.Component {
+  render() {
+    console.log(this.props);
+    return (
+      <div>
+        <input type="text" />
+        <button>click</button>
+        <ul>
+          {this.props.testStore.map((item) => (
+            <li key={item}>{item}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 // connect - это декоратор - функция, выполняющая дополнительные действия вокруг вызова основной функции
 // у коннекта два параметра: mapStateToProps и mapDispatchToProps
--- src/index.js
+++ src/index.js
@@ -3,42 +3,42 @@ import ReactDOM from "react-dom"; // для старой концепции
 // import { createRoot } from "react-dom/client"; // для новой концепции
 
 // ВС-код ругается на древность криэйтСтора, пока не нашел, на что заменить
-import { createStore } from "redux";
+import { legacy_createStore as createStore } from "redux";
 import { Provider } from "react-redux";
 
 import App from "./components/App.jsx";
 
-// объявление стора с указанием метода, которым он будет обрабатывать экшены
-const store = createStore(changeStore);
 const initialState = ["redux", "react"];
 
 // поиск нереактовых компонентов для показа работы редукса вне реакта
 // они у нас в index.html расположены за границами рутового элемента
-const testUl = document.querySelector(".test-ul");
-const testButton = document.querySelector(".test-button");
-const testInput = document.querySelector(".test-input");
+// const testUl = document.querySelector(".test-ul");
+// const testButton = document.querySelector(".test-button");
+// const testInput = document.querySelector(".test-input");
 
-// реализация метода стора, оборабатываюющего экшены
+// редьюсер - реализация метода стора, оборабатываюющего экшены
 function changeStore(state = initialState, action) {
   switch (action.type) {
     case "WRITE":
       return [...state, action.payload];
-      break;
     default:
       return state;
   }
 }
 
+// объявление стора с указанием метода, которым он будет обрабатывать экшены
+const store = createStore(changeStore);
+
 // это типа подписка компонента на обновление стора
-store.subscribe(() => {
-  testUl.innerHTML = "";
-  testInput.value = "";
-  store.getState().map((item) => {
-    const li = document.createElement("li");
-    li.textContent = item;
-    testUl.appendChild(li);
-  });
-});
+// store.subscribe(() => {
+//   testUl.innerHTML = "";
+//   testInput.value = "";
+//   store.getState().map((item) => {
+//     const li = document.createElement("li");
+//     li.textContent = item;
+//     testUl.appendChild(li);
+//   });
+// });
 
 // при нажатии на кнопку формируется экшен, которое говорит стору внести запись
 // testButton.addEventListener("click", () => {
